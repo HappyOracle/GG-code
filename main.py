66954202db5a572613d7b294d55c6d1f4b906c6e
Dr. AInme <109267676+HappyOracle@users.noreply.github.com>
2022-08-08 15:31:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# GG-code
+
 from telethon import TelegramClient
 from array import *
 from telethon.tl.functions.channels import GetFullChannelRequest
@@ -7,37 +9,37 @@ from telethon import TelegramClient, events
 
 
 
-api_id = 17876697
-api_hash = 'f57224fa791de500d30a17a4fbb650ec'
+api_id = 123
+api_hash = '123'
 client = TelegramClient('anon', api_id, api_hash)
 chat = ''
 #print(api_id, ) # типа сперва появляется ID и никнейм, а после perepiska
 client.start()
 
 
-@client.on(events.NewMessage(chats=(-1001299652068)))
+@client.on(events.NewMessage(chats=(123)))
 async def main(event):
     f = open("telethon.txt", "a+", encoding='utf-16' '\n') #encoding='utf-8' или 'utf-16' - это байт - '\n'
-    async for message in client.iter_messages(-1001299652068):
+    async for message in client.iter_messages(123):
         from_user = await client.get_entity(message.from_id.user_id)
         print(from_user.username)
         print(message.id, message.text) #bio \n'
         #mess_date = event.newmessage.to_dict()['date'] #ЫЫЫЫ ЕСЛИ ЧЕ УДАЛИ
-        #print(client.get_entity(-1001299652068).stringify()) #фигово работает
+        #print(client.get_entity(123).stringify()) #фигово работает
         #chat = await client.get_entity(message.chat_id)  # Получаем чат по ИД
         #print(chat.username)
         async for dialog in client.iter_dialogs(): #работает, но фигово
-            entity = await client.get_entity(-1001299652068)
+            entity = await client.get_entity(123)
             print(dialog.name, dialog.id)
-        async for user in client.iter_participants(chat, filter=-1001299652068):  # dont work
+        async for user in client.iter_participants(chat, filter=123):  # dont work
             print(user.id)
         msg_text = "{User}: {dialog} {id} {Message}  \n".format(User=message.id, dialog=dialog.id, id=user.id, Message=message.text) #user.id mess_date=mess_date,
         f.write(msg_text)
         #dialogs = await client.get_dialogs() #pizda
         #print(dialogs) #gg
-        #async for user in client.iter_participants(chat, filter=-1001299652068):  # dont work
+        #async for user in client.iter_participants(chat, filter=123):  # dont work
         #    print(user.id)
-        #entity = await client.get_entity(-1001299652068) #работает, но фигово
+        #entity = await client.get_entity(123) #работает, но фигово
         #print(entity)
         if message.photo:
             print('File Name :' + str(message.file.name))
